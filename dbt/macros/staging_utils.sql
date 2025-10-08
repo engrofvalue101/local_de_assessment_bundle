9e8cc90d2882ec4f7f4a21f3131be8525cd70898
Lorenzo <johnlore.alay-ay@insight.com>
2025-10-08 14:53:28 +0800

    Modified the macros

--- dbt/macros/staging_utils.sql
+++ dbt/macros/staging_utils.sql
@@ -6,7 +6,7 @@
 {% macro safe_cast(column, data_type) %}
     TRY_CAST({{ column }} AS {{ data_type }})
 {% endmacro %}
-
+git push 
 {% macro convert_to_utc(column_name) %}
     timezone('UTC', {{ column_name }})
 {% endmacro %}
